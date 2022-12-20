e37455bad0d33c1cae62edb5996771ec11bc8cfe
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2022-12-20 15:25:43 +0200

    made host and ports as global variables

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import socket
 from datetime import datetime
 import json
 
+
+HOST = '127.0.0.1'
+SERVER_PORT = 3000
+CLIENT_PORT = 5000
+
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def send_html_file(self, filename: str, status: int = 200):
@@ -29,16 +34,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                      self.send_static()
                 else:
                     self.send_html_file('error.html', 404)
-
-        # if url == '/':
-        #     self.send_html_file('index.html')
-        # elif url == '/message':
-        #     self.send_html_file('message.html') 
-        # else:
-        #     if pathlib.Path().joinpath(url[1:]).exists():
-        #          self.send_static()
-        #     else:
-        #         self.send_html_file('error.html', 404)
         
 
     def do_POST(self): 
@@ -50,13 +45,12 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def send_data(self, data: bytes):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.connect(('127.0.0.1', 5000))
+        sock.connect(('127.0.0.1', CLIENT_PORT))
         sock.send(data)
 
     def send_static(self):
         self.send_response(200) 
         mt = mimetypes.guess_type(url=self.path)
-        print(mt)
         if mt:
             self.send_header("Content-type", mt[0])
         else:
@@ -65,8 +59,8 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         with open(f'.{self.path}', 'rb') as file:
             self.wfile.write(file.read())
 
-def run_web_server(HOST: str = '127.0.0.1', PORT: int=3000):
-    httpd = HTTPServer((HOST, PORT), SimpleHTTPRequestHandler)
+def run_web_server():
+    httpd = HTTPServer((HOST, SERVER_PORT), SimpleHTTPRequestHandler)
     try:
         httpd.serve_forever()
         print('Running web server...')
@@ -74,10 +68,10 @@ def run_web_server(HOST: str = '127.0.0.1', PORT: int=3000):
         httpd.server_close()
             
 
-def run_socket_server(HOST: str = '127.0.0.1', PORT: int=5000):
+def run_socket_server():
 
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server.bind((HOST, PORT))
+    server.bind((HOST, CLIENT_PORT))
     try:
         while True:
             data, address = server.recvfrom(1024)
@@ -92,7 +86,6 @@ def run_socket_server(HOST: str = '127.0.0.1', PORT: int=5000):
 def write_in_json(data: bytes):
     query_data: str = unquote_plus(data.decode())
     data: dict = {key: value for key, value in [el.split('=') for el in query_data.split('&')]}
-    # print(f'Parsed data = {data}')
     new_data: dict = {str(datetime.now()): data}
     
     json_data: dict = json.load(open('storage\data.json'))
@@ -106,8 +99,8 @@ def write_in_json(data: bytes):
 
 
 if __name__ == '__main__':
-    web_server = Thread(target=run_web_server, args=('127.0.0.1', 3000))
-    socket_server = Thread(target=run_socket_server, args=('127.0.0.1', 5000))
+    web_server = Thread(target=run_web_server)
+    socket_server = Thread(target=run_socket_server)
 
     web_server.start()
     socket_server.start()
